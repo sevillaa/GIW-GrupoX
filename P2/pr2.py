0668a6a9352f70708f22ff81f548f086714a4308
echo1 <149362942+IzanDeVegaLopez@users.noreply.github.com>
2026-09-24 21:49:33 +0200

    P2.1 completo renamed file, ej1 finished

--- P2/pr2.py
+++ P2/pr2.py
@@ -15,17 +15,20 @@ resultados de los demás.
 """
 
 # https://docs.python.org/3/library/csv.html
-import csv;
-import json;
+import csv
+
 from pprint import pprint
 
 ### Formato CSV
 def lee_fichero_accidentes(ruta):
-    '''Lee un fichero csv con delimitador ';' y en el que la cabecera marca las claves de un diccionario
-    y los valores vienen dados por los valores en la misma posición de las siguientes filas.
+    '''Lee un fichero csv con delimitador ';' y en el que la cabecera marca las claves de un 
+    diccionario y los valores vienen dados por los valores en la misma posición de las 
+    siguientes filas. 
     De forma que devuelve un array de diccionarios a partir del fichero en 'ruta' 
     '''
     with open(ruta, "r", newline='', encoding='utf8') as fich:
+        # DictReader reads the csv as a dictionary.
+        # Keys are first row, values of each key are in the rest of rows in the same idx as the key
         lector = csv.DictReader(fich, delimiter=';')
         whole_list = list(lector)
         return whole_list
@@ -33,13 +36,14 @@ def lee_fichero_accidentes(ruta):
 def accidentes_por_distrito_tipo(datos):
     '''given a list of dictionary returns the number of accidents of each type on each district
     This uses keys: "distrito" and "tipo_accidente" and returns a dictionary whose keys are
-    ("distrito","tipo_accidente") and value is the number of "tipo_accidente" in "distrito" in "datos"
+    ("distrito","tipo_accidente") and value is the number of "tipo_accidente" 
+    in "distrito" in "datos"
     '''
     ret_val = {}
     for val in datos:
-        if not (val["distrito"],val["tipo_accidente"]) in ret_val:
+        if (val["distrito"],val["tipo_accidente"]) not in ret_val:
             ret_val[(val["distrito"],val["tipo_accidente"])] = 1
-        else: 
+        else:
             ret_val[(val["distrito"], val["tipo_accidente"])] += 1
     return ret_val
 
@@ -55,25 +59,37 @@ def dias_mas_accidentes(datos):
         if not val["fecha"] in ret_val:
             ret_val[val["fecha"]] = 1
             greatest_value = max(greatest_value, 1)
-        else: 
+        else:
             ret_val[val["fecha"]] += 1
             greatest_value = max(ret_val[val["fecha"]], greatest_value)
 
-    return {key:value for key,value in ret_val.items() if value == greatest_value}
+    return [(key,value) for key,value in ret_val.items() if value == greatest_value]
 
 def puntos_negros_distrito(datos, distrito, k):
-    ...
-
+    '''returns a list of pairs. The first element of the pair contains the concrete point 
+    of the accident. The second contains the number of incidents in that concrete point. 
+    Returns the top k pairs.
+    Ordering in descending order by number of accidents, and alphabetically in case of draw'''
+    ret_val={}
+    for val in datos:
+        if val["distrito"] == distrito:
+            if val["localizacion"] not in ret_val:
+                ret_val[val["localizacion"]] = 0
+            else:
+                ret_val[val["localizacion"]] += 1
+    # items() returns a pair(key,value) dictionary view
+    # its connected (shares elements) to the dictionary, that's why we gotta use 'list()'
+    array_ret_val = list(ret_val.items())
+    # orders by descendant value and alphabetically in case of draw
+    array_ret_val.sort(key=lambda x: (-x[1], x[0]))
+    return [array_ret_val[i] for i in range(0,k)]
 
 #### Formato JSON
 def leer_monumentos(ruta):
-    
     with open(ruta, 'r', encoding='utf-8') as archivo:
         datos = json.load(archivo)
     print(datos)
 
-    
-    
 
 def codigos_postales(monumentos):
     ...
@@ -85,13 +101,15 @@ def busqueda_distancia(monumentos, direccion, distancia):
     ...
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    #data = lee_fichero_accidentes("AccidentesBicicletas_2025.csv")
-    #pprint(accidentes_por_distrito_tipo(data))
-    
-    leer_monumentos("300356-2-monumentos-ciudad-madrid-json.json")
-=======
+    pprint("Ejercicio 1:")
+
     data = lee_fichero_accidentes("AccidentesBicicletas_2025.csv")
-    # pprint(accidentes_por_distrito_tipo(data))
+    pprint("Accidentes por distrito: ")
+    pprint(accidentes_por_distrito_tipo(data))
+    pprint("Dias con más accidentes: ")
     pprint(dias_mas_accidentes(data))
->>>>>>> e2dc3d9d65c842aa55636089f04aa0bba31a5e1e
+    pprint("Top 5 puntos negros Centro")
+    pprint(puntos_negros_distrito(data, "CENTRO", 5))
+
+    pprint("Ejercicio 2:")
+    # leer_monumentos("300356-2-monumentos-ciudad-madrid-json.json")
