cd912a9f73249910f451e61dd51c98f0a4252906
Adriii31 <adrimu07@ucm.es>
2026-09-24 14:36:24 +0200

    Add files via upload

---
+++ P3/pr3_skel.py
@@ -0,0 +1,32 @@
+"""
+TODO: rellenar
+
+Asignatura: GIW
+Práctica 3
+Grupo: XXXXXXX
+Autores: XXXXXX 
+
+Declaramos que esta solución es fruto exclusivamente de nuestro trabajo personal. No hemos
+sido ayudados por ninguna otra persona o sistema automático ni hemos obtenido la solución
+de fuentes externas, y tampoco hemos compartido nuestra solución con otras personas
+de manera directa o indirecta. Declaramos además que no hemos realizado de manera
+deshonesta ninguna otra actividad que pueda mejorar nuestros resultados ni perjudicar los
+resultados de los demás.
+"""
+
+
+
+def nombres_restaurantes(filename):
+    ...
+
+
+def subcategorias(filename):
+    ...
+
+
+def info_restaurante(filename, name):
+    ...
+
+
+def busqueda_cercania(filename, lugar, n):
+    ...
