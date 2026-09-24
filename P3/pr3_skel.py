f63159a89c1e44eeb7e3f6210e998abb7cd03046
Adriii31 <adrimu07@ucm.es>
2026-09-24 14:59:51 +0200

    Empezada practica 3

--- P3/pr3_skel.py
+++ P3/pr3_skel.py
@@ -3,8 +3,8 @@ TODO: rellenar
 
 Asignatura: GIW
 Práctica 3
-Grupo: XXXXXXX
-Autores: XXXXXX 
+Grupo: 10
+Autores: Adrián Muñoz Rodríguez 
 
 Declaramos que esta solución es fruto exclusivamente de nuestro trabajo personal. No hemos
 sido ayudados por ninguna otra persona o sistema automático ni hemos obtenido la solución
@@ -14,10 +14,29 @@ deshonesta ninguna otra actividad que pueda mejorar nuestros resultados ni perju
 resultados de los demás.
 """
 
+import html
+import xml.sax
 
+class _HandlerRestaurante(xml.sax.ContentHandler):
 
-def nombres_restaurantes(filename):
-    ...
+    def __init__(self): #Inicializador de la clase
+        super().__init__()
+        self.in_name = False #bandera para saber si estamos dentro de <name>
+        self.current_text  = [] #acumulador nom
+        self.names = [] #lista final
+
+    def startElement(self, name, attrs):
+
+        #el dataset usa <name> para cada nombre en <basicData>
+        if name.lower() == "name":
+            self.in_name = True
+            self.current_text = []
+
+    def characters(self, content):
+        if self.in_name:
+            self.current_text.append(content)
+
+    
 
 
 def subcategorias(filename):
